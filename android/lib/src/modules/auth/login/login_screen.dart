67215d87018628efe6c6fc942fd2f10892d9063b
farhanroy <farhanroy120@gmail.com>
2021-02-07 07:02:55 +0700

    feature: login

---
+++ android/lib/src/modules/auth/login/login_screen.dart
@@ -0,0 +1,26 @@
+import 'package:bade_wangsul/src/components/input.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              StreamBuilder(
+                stream: ,
+                  builder: (context, snapshot){
+
+                  })
+            ],
+          ),
+        )
+    );
+  }
+}
