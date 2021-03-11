1bc1e37ef63ded32643cf7b704ed4ae3d3fe054f
farhanroy <farhanroy120@gmail.com>
2021-03-11 14:05:04 +0700

    :pencil2: lanjut create izin & list izin pengasuh

---
+++ android/lib/src/modules/pengasuh/izin/view/list_izin_page.dart
@@ -0,0 +1,63 @@
+import 'package:bade_wangsul/src/utils/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ListIzinPagex extends StatelessWidget {
+  final CollectionReference _ref =
+  FirebaseFirestore.instance.collection(Constants.IZIN_COLLECTION);
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _ref.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return SingleChildScrollView(
+            child: Column(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return _ItemIzinSantri(idSantri: document.data()['idSantri'],);
+              }).toList(),
+            ),
+          );
+        }
+    );
+  }
+}
+
+class _ItemIzinSantri extends StatelessWidget {
+  _ItemIzinSantri({Key key, this.idSantri}) : super(key: key);
+
+  final String idSantri;
+  final CollectionReference ref = FirebaseFirestore.instance
+      .collection(Constants.SANTRI_COLLECTION);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: ref.doc(idSantri).get(),
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return new ListTile(
+          leading: CircleAvatar(
+            backgroundImage:
+            NetworkImage("${snapshot.data.data()["imageUrl"]}"),
+            backgroundColor: Colors.transparent,
+          ),
+          title: new Text(snapshot.data.data()['name']),
+          subtitle: new Text(snapshot.data.data()['dormitory']),
+        );
+      },
+    );
+  }
+}
---
+++ android/lib/src/utils/validator/date_validator.dart
@@ -0,0 +1,13 @@
+import 'package:formz/formz.dart';
+
+enum DateValidationError { invalid }
+
+class Date extends FormzInput<DateTime, DateValidationError> {
+  const Date.pure() : super.pure(null);
+  const Date.dirty([DateTime value]) : super.dirty(value);
+
+  @override
+  DateValidationError validator(DateTime value) {
+    return value != null ? null : DateValidationError.invalid;
+  }
+}
